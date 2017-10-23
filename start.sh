26707e3bcd7036d7ebe800b85120f4547aca1354
roarteaga <32908784+roarteaga@users.noreply.github.com>
2017-10-23 15:24:16 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 ﻿docker rmi roarteaga/serviciosuma;
 docker pull "roarteaga/serviciosuma";
 docker run -d -t -p 32768:82 --name serviciosuma roarteaga/serviciosuma;
-echo 'Ejecute el serivio de la siguiente manera:'
-echo 'http://0.0.0.0:32768/api/sum?num1=1&num2=22'
+echo 'Ejecute el serivio de la siguiente manera:';
+echo 'http://0.0.0.0:32768/api/sum?num1=1&num2=22';
